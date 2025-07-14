6d39007c513134ef82b949680f5f64555d9c2ac3
yaowenqiang <yaowenqiang111@163.com>
2025-07-14 23:54:48 +0800

    add floats, bool and bool, char demo

--- variables_and_types.rs
+++ variables_and_types.rs
@@ -16,6 +16,8 @@
 
 fn main() {
     demo_integers();
+    demo_floats();
+    demo_other_simple_types();
 }
 
 fn demo_integers() {
@@ -35,4 +37,44 @@ fn demo_integers() {
         "isize is {} bytes on my machine.",
         std::mem::size_of::<isize>()
     );
+}
+
+// f32
+// f64
+
+fn demo_floats() {
+    let f1: f32 = 1.2345;
+    let f2: f64 = 9.87654;
+    println!("\n Floats are {} {}", f1, f2);
+    println!("\n Floats field width 10 L-aligned filled with space are \n***{:<10.2}*** and ***{:<10.2}***", f1, f2);
+    println!("\n Floats field width 10 R-aligned filled with space are \n***{:>10.2}*** and ***{:>10.2}***", f1, f2);
+    println!("\n Floats field width 10 L-aligned filled with tilde are \n***{:~<10.2}*** and ***{:~<10.2}***", f1, f2);
+    println!("\n Floats field width 10 R-aligned filled with tilde are \n***{:~>10.2}*** and ***{:~>10.2}***", f1, f2);
+
+    let f3: f32 = -1.6021763e-16;
+    let f4: f64 = 2.99792458e8;
+    println!("\nElectron charge {0}, {0:e}, {0:.4e}", f3);
+    println!("\nSpeed of light {0}, {0:e}, {0:.4e}", f4);
+}
+
+fn demo_other_simple_types() {
+    let is_welsh: bool = true;
+    let can_sign: bool = false;
+    println!("\nIs welsh? {}, can sign? {}", is_welsh, can_sign);
+
+    let is_welsh_num: i32 = is_welsh as i32;
+    let can_sign_num: i32 = can_sign as i32;
+
+    println!(
+        "\nIs Welsh as number: {}, can sign as number: {}",
+        is_welsh_num, can_sign_num
+    );
+
+    let res1: bool = is_welsh && can_sign;
+    let res2: bool = is_welsh || can_sign;
+    let res3: bool = !(is_welsh || can_sign);
+    print!("\n res1:{}, res2: {}, res3: {}", res1, res2, res3);
+
+    let middle_initial: char = 'c';
+    print!("\nhey you with the middle initial {}", middle_initial);
 }
