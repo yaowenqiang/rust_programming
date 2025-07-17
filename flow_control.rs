eb512e5b46690e11589e38cc112e07179ee0f646
yaowenqiang <yaowenqiang111@163.com>
2025-07-17 23:52:41 +0800

    add match expression demo

--- flow_control.rs
+++ flow_control.rs
@@ -1,5 +1,6 @@
 fn main() {
     demo_if();
+    demo_match();
 }
 
 fn demo_if() {
@@ -28,4 +29,40 @@ fn demo_if() {
 
     let msg = if age > 50 { "old" } else { "young" };
     print!("You are {}", msg);
+}
+
+fn demo_match() {
+    let num = 100;
+    println!("\nUsing a match to test an expression against patterns");
+
+    match num {
+        100 => println!("A hundred"),
+        200 => println!("Two hundred"),
+        _ => print!("Something else"),
+    }
+
+    match num {
+        25..=50 => println!("25 to 50"),
+        51..=100 => println!("51 to 100"),
+        _ => print!("Something else"),
+    }
+    match num {
+        25 | 50 | 75 => println!("25, 50, or 7t"),
+        100 | 200 => println!("100 or 200"),
+        _ => println!("Something else"),
+    }
+
+    match num {
+        x if x < 50 => println!("less then 50"),
+        x if x == 75 => println!("75"),
+        _ => println!("Something else"),
+    }
+    let res = match num {
+        x if x < 50 =>"less then 50",
+       x if x == 75 => "75",
+        _ => "Something else"
+    };
+
+
+    println!("Result of match expression {}", res)
 }
