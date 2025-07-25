e4e3b9bd0fb59186f058593e4eb30d2d63653114
yaowenqiang <yaowenqiang111@163.com>
2025-07-25 23:34:05 +0800

    add more array demo

--- arrays_demo.rs
+++ arrays_demo.rs
@@ -1,5 +1,7 @@
 fn main() {
-    demo_arrays()
+    demo_arrays();
+    demo_arrays_techniques();
+        
 }
 
 fn demo_arrays(){
@@ -10,4 +12,23 @@ fn demo_arrays(){
     let mut a2 = [100, 101, 102];
     a2[0] = 999;
     println!("a2 length is {}, first element is {}",a2.len(), a2[0]);
+
+    println!("Elements in a2:");
+    for elem in a2 {
+        println!(" {}", elem);
+    }
 }
+
+fn demo_arrays_techniques(){
+    println!("\nArray techniques");
+    let a1:[i64; 5];
+    a1 = [100, 101, 102, 103, 104];
+    println!("a1 is {:?}", a1);
+
+    let mut a2 = [99; 5];
+    a2[0] = 50;
+    a2[4] = 25;
+    println!("a2 is {:?}", a2);
+
+}
+
