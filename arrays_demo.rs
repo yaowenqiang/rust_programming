b6b01be7336e18087a09e6ee3fc6eb221f07b20e
yaowenqiang <yaowenqiang111@163.com>
2025-07-26 23:48:51 +0800

    fix more bugs

--- arrays_demo.rs
+++ arrays_demo.rs
@@ -1,6 +1,7 @@
 fn main() {
     demo_arrays();
     demo_arrays_techniques();
+    demo_tup0les()
         
 }
 
@@ -32,3 +33,23 @@ fn demo_arrays_techniques(){
 
 }
 
+
+fn demo_tup0les() {
+    println!("\nUsing tuples");
+    let t1 = (9, "HI", 3.5);
+    println!("t1 elements are {},{},{}", t1.0, t1.1,t1.2);
+
+    let mut t2 = (9,"hi", 3.5);
+    t2.0 = 99;
+    println!("t2 elements are {},{},{}", t2.0, t2.1,t2.2);
+
+    let t3 = ();
+    println!("t3 is {:?}", t3);
+
+    let t4: (i32, bool, f64);
+
+    t4 = (50, true, 1.67);
+
+    println!("t4 is {:?}, elements are {}, {},{}", t4,  t4.0, t4.1, t4.2);
+
+}
