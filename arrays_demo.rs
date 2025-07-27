0de26bd22be4aeba69029c949414046843722b4d
yaowenqiang <yaowenqiang111@163.com>
2025-07-27 23:40:07 +0800

    add vectors demo

--- arrays_demo.rs
+++ arrays_demo.rs
@@ -1,18 +1,18 @@
 fn main() {
     demo_arrays();
     demo_arrays_techniques();
-    demo_tup0les()
-        
+    demo_tup0les();
+    demo_vectors();
 }
 
-fn demo_arrays(){
+fn demo_arrays() {
     println!("\nDemo arrays");
     let a1 = [100, 101, 102];
     println!("a1 length is {}, first element is {}", a1.len(), a1[0]);
 
     let mut a2 = [100, 101, 102];
     a2[0] = 999;
-    println!("a2 length is {}, first element is {}",a2.len(), a2[0]);
+    println!("a2 length is {}, first element is {}", a2.len(), a2[0]);
 
     println!("Elements in a2:");
     for elem in a2 {
@@ -20,9 +20,9 @@ fn demo_arrays(){
     }
 }
 
-fn demo_arrays_techniques(){
+fn demo_arrays_techniques() {
     println!("\nArray techniques");
-    let a1:[i64; 5];
+    let a1: [i64; 5];
     a1 = [100, 101, 102, 103, 104];
     println!("a1 is {:?}", a1);
 
@@ -30,18 +30,16 @@ fn demo_arrays_techniques(){
     a2[0] = 50;
     a2[4] = 25;
     println!("a2 is {:?}", a2);
-
 }
-
 
 fn demo_tup0les() {
     println!("\nUsing tuples");
     let t1 = (9, "HI", 3.5);
-    println!("t1 elements are {},{},{}", t1.0, t1.1,t1.2);
+    println!("t1 elements are {},{},{}", t1.0, t1.1, t1.2);
 
-    let mut t2 = (9,"hi", 3.5);
+    let mut t2 = (9, "hi", 3.5);
     t2.0 = 99;
-    println!("t2 elements are {},{},{}", t2.0, t2.1,t2.2);
+    println!("t2 elements are {},{},{}", t2.0, t2.1, t2.2);
 
     let t3 = ();
     println!("t3 is {:?}", t3);
@@ -50,6 +48,33 @@ fn demo_tup0les() {
 
     t4 = (50, true, 1.67);
 
-    println!("t4 is {:?}, elements are {}, {},{}", t4,  t4.0, t4.1, t4.2);
+    println!("t4 is {:?}, elements are {}, {},{}", t4, t4.0, t4.1, t4.2);
+}
 
+fn demo_vectors() {
+    println!("\nUsing vectors");
+
+    let mut _v1: Vec<i32> = Vec::new();
+    let mut _v2 = Vec::<i32>::new();
+
+    let mut _v3 = vec![100, 101, 102];
+
+    let item = _v3[0];
+    println!("Value: {}", item);
+
+    let opt = _v3.get(0);
+    match opt {
+        Some(value) => println!("Value: {}", value),
+        None => println!("No value"),
+    }
+    _v3.push(103);
+    _v3.push(104);
+    _v3.push(105);
+    _v3.push(106);
+    _v3.pop();
+    _v3.insert(0, 99);
+    println!("Items in v3:");
+    for item in _v3 {
+        println!(" {}", item);
+    }
 }
