0e267a2ccbfb47ca7fd4e9b6c0f851b38e35c3a6
yaowenqiang <yaowenqiang111@163.com>
2025-08-08 23:46:19 +0800

    add more string slice demo

--- string_slices.rs
+++ string_slices.rs
@@ -21,6 +21,28 @@ fn main() {
     let message = "今天天气不错";
     // byte index
     //
-    let m = &message[0 .. 3];
+    let m = &message[0..3];
     println!("{}", m);
+
+    let s3 = &message[0..3];
+    let s4 = &message[..3];
+    let s5 = &message[3..6];
+    let s6 = &message[3..];
+    println!(
+        "\ns3 ptr: {:p}, len: {}, text: {}",
+        s3.as_ptr(),
+        s3.len(),
+        s3
+    );
+    println!("s4 ptr: {:p}, len: {}, text: {}", s4.as_ptr(), s4.len(), s4);
+    println!("s5 ptr: {:p}, len: {}, text: {}", s5.as_ptr(), s5.len(), s5);
+    println!("s6 ptr: {:p}, len: {}, text: {}", s6.as_ptr(), s6.len(), s6);
+
+    // mutable string slices
+
+    let mut message2 = String::from("mutable string");
+    let ms: &mut str = &mut message2[9..];
+    ms.make_ascii_uppercase();
+    println!("{}", message2);
+    //println!("{}", ms);
 }
