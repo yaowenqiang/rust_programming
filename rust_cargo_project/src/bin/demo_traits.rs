b83fc56989d137fafd5b594d36668f45db82c10b
yaowenqiang <yaowenqiang111@163.com>
2025-08-31 23:34:01 +0800

    add Polymorphism demo

--- rust_cargo_project/src/bin/demo_traits.rs
+++ rust_cargo_project/src/bin/demo_traits.rs
@@ -1,3 +1,4 @@
+use std::fmt::Pointer;
 use chrono::Utc;
 
 trait Print {
@@ -15,6 +16,11 @@ trait Log {
         self.log();
         println!("-----------------------");
     }
+}
+
+struct Point {
+    x: i32,
+    y: i32,
 }
 struct Employee {
     name: String,
@@ -47,7 +53,25 @@ impl Print for Employee {
         );
     }
 }
+
+impl Print for Point {
+    fn print(&self) {
+        println!(
+            "x: {} , y: {}",
+            self.x, self.y
+        );
+    }
+}
+
+fn print_something(p: &dyn Print) {
+    println!("from print_something");
+    p.print();
+}
 fn main() {
     let employee = Employee::new("name".to_string(), 10, false);
     employee.print();
+    let obj1 = Employee::new("name".to_string(), 10, true);
+    let obj2 :Point = Point { x: 10, y: 10 };
+    print_something(&obj1);
+    print_something(&obj2);
 }
