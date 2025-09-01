a20110cfad3413aa5c7d93e9903d2456e92c0e3c
yaowenqiang <yaowenqiang111@163.com>
2025-09-01 23:30:57 +0800

    add trait inheritance demo

--- rust_cargo_project/src/bin/demo_traits.rs
+++ rust_cargo_project/src/bin/demo_traits.rs
@@ -1,8 +1,54 @@
-use std::fmt::Pointer;
 use chrono::Utc;
+use std::collections::VecDeque;
+use std::fmt::{Display, Pointer};
 
 trait Print {
     fn print(&self);
+}
+
+trait Queue {
+    fn len(&self) -> usize;
+    fn push_back(&mut self, m: i32);
+    fn pop_front(&mut self, m: i32) -> Option<i32>;
+}
+
+trait Deque: Queue {
+    fn push_front(&mut self, m: i32);
+    fn pop_back(&mut self) -> Option<i32>;
+}
+
+struct MyDeque {
+    data: VecDeque<i32>,
+}
+
+impl MyDeque {
+    pub fn new() -> MyDeque {
+        let data = VecDeque::<i32>::new();
+        MyDeque { data }
+    }
+}
+
+impl Queue for MyDeque {
+    fn len(&self) -> usize {
+        self.data.len()
+    }
+
+    fn push_back(&mut self, m: i32) {
+        self.data.push_back(m);
+    }
+    fn pop_front(&mut self, m: i32) -> Option<i32> {
+        self.data.pop_front()
+    }
+}
+
+impl Deque for MyDeque {
+    fn push_front(&mut self, m: i32) {
+        self.data.push_front(m);
+    }
+
+    fn pop_back(&mut self) -> Option<i32> {
+        self.data.pop_back()
+    }
 }
 
 trait Log {
@@ -40,9 +86,9 @@ impl Employee {
 
 impl Log for Employee {
     const LOG_TIMESTAMP: bool = true;
-   fn log(&self) {
+    fn log(&self) {
         println!("{},{},{}", self.name, self.salary, self.fulltime);
-   }
+    }
 }
 
 impl Print for Employee {
@@ -56,10 +102,7 @@ impl Print for Employee {
 
 impl Print for Point {
     fn print(&self) {
-        println!(
-            "x: {} , y: {}",
-            self.x, self.y
-        );
+        println!("x: {} , y: {}", self.x, self.y);
     }
 }
 
@@ -71,7 +114,24 @@ fn main() {
     let employee = Employee::new("name".to_string(), 10, false);
     employee.print();
     let obj1 = Employee::new("name".to_string(), 10, true);
-    let obj2 :Point = Point { x: 10, y: 10 };
+    let obj2: Point = Point { x: 10, y: 10 };
     print_something(&obj1);
     print_something(&obj2);
+
+    println!("\n demo trait inheritance");
+    let mut d = MyDeque::new();
+    d.push_back(300);
+    d.push_back(400);
+    d.push_back(500);
+    d.push_front(200);
+    d.push_front(100);
+
+    println!("MyQueue object has {} items", d.len());
+
+    loop {
+        match d.pop_back() {
+            Some(v) => println!("{}", v),
+            None => break,
+        }
+    }
 }
