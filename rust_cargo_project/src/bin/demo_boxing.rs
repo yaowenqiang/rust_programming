b08b8890056b7d20c9dafeba0dc857e8628a3b6c
yaowenqiang <yaowenqiang111@163.com>
2025-09-07 23:19:24 +0800

    add Box chain demo

--- rust_cargo_project/src/bin/demo_boxing.rs
+++ rust_cargo_project/src/bin/demo_boxing.rs
@@ -2,6 +2,70 @@ struct Employee {
     name: String,
     salary: u64,
 }
+struct Node {
+    data: i32,
+    next: Option<Box<Node>>,
+}
+
+impl Node {
+    fn new(data: i32) -> Node {
+        Node { data, next: None }
+    }
+    fn append(&mut self, data: i32) {
+        match self.next {
+            None => {
+                self.next = Some(Box::new(Node::new(data)));
+            }
+            Some(ref mut boxed_next_node) => {
+                boxed_next_node.append(data);
+            }
+        }
+    }
+
+    fn display(&self) {
+        println!("{}", self.data);
+        match self.next {
+            None => {
+                println!("[End]");
+            }
+            Some(ref boxed_next_node) => {
+                boxed_next_node.display();
+            }
+        }
+    }
+}
+
+struct Chain {
+    head: Option<Box<Node>>,
+}
+
+impl Chain {
+    fn new() -> Chain {
+        Chain { head: None }
+    }
+
+    fn insert(&mut self, data: i32) {
+        match self.head {
+            None => {
+                self.head = Some(Box::new(Node::new(data)));
+            }
+            Some(ref mut boxed_head_node) => {
+                boxed_head_node.append(data);
+            }
+        }
+    }
+
+    fn display(&self) {
+        match self.head {
+            None => {
+                println!("[Empty chain]");
+            }
+            Some(ref boxed_head_node) => {
+                boxed_head_node.display();
+            }
+        }
+    }
+}
 
 fn main() {
     let boxed_number = Box::new(42);
@@ -12,6 +76,13 @@ fn main() {
     println!("Value of value: {}", value);
 
     do_it();
+
+    let mut chain = Chain::new();
+    chain.insert(42);
+    chain.insert(11);
+    chain.insert(22);
+    chain.insert(33);
+    chain.display();
 }
 
 fn do_it() {
